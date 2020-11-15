ef553e839ff7a76ce5fd93da12fb84e4fc9406db
Benoît Cante <cantebenoit@hotmail.com>
2020-11-15 09:18:16 +0100

    Changement du none en ground sur un clic

--- Assets/4 - TileMap/Scripts/Testing.cs
+++ Assets/4 - TileMap/Scripts/Testing.cs
@@ -7,46 +7,20 @@ namespace Tilemap_4
 {
     public class Testing : MonoBehaviour
     {
-        //private Pathfinding pathfinding;
-        //[SerializeField]
-        //private HeatMapGenericVisual heatMapGenericVisual;
-        //private void Start()
-        //{
+        Tilemap tilemap;
+        private void Start()
+        {
 
-        //    pathfinding = new Pathfinding(10, 10);
-        //    Grid<PathNode> g = pathfinding.GetGrid();
-        //    int width = g.GetWidth();
-        //    for (int x = 0; x < width - 1; x ++) { 
-        //        g.GetGridObject(x, 1).nodeType = NodeType.Wall;
-        //        g.GetGridObject(width - 1 - x, 3).nodeType = NodeType.Wall;
-        //        g.GetGridObject(x, 5).nodeType = NodeType.Wall;
-        //    }
-        //    heatMapGenericVisual.SetGrid(g);
-        //}
+            tilemap = new Tilemap(10, 20, 10f, Vector3.zero);
+        }
 
         private void Update()
         {
-           //if (Input.GetMouseButtonDown(0))
-           // {
-           //     Vector3 mouseWorldPosition = UtilsClass.GetMouseWorldPosition();
-           //         pathfinding.GetGrid().GetXY(mouseWorldPosition, out int x, out int y);
-
-
-           //     if (pathfinding.GetGrid().ClickInRange(x,y))
-           //     {
-           //          List<PathNode> path = pathfinding.FindPath(0, 0, x, y);
-           //         if (path != null)
-           //         {
-           //             for (int i = 0; i < (path.Count-1); i++)
-           //             {
-           //                 Debug.DrawLine(new Vector3(path[i].x, path[i].y) * 10f + Vector3.one * 5f, 
-           //                     new Vector3(path[i + 1].x, path[i + 1].y) * 10f + Vector3.one * 5f, Color.green, 100);
-           //             }
-           //         }
-           //     }
-                
-
-           // }
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 mouseWorldPosition = UtilsClass.GetMouseWorldPosition();
+                tilemap.SetTilemapSprite(mouseWorldPosition, TilemapObject.TilemapSprite.Ground);
+            }
         }
 
     }
---
+++ Assets/4 - TileMap/Scripts/Tilemap.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tilemap_4
+{
+    public class Tilemap : MonoBehaviour
+    {
+        private Grid<TilemapObject> grid;
+
+        public Tilemap(int width, int height, float cellSize, Vector3 originPosition)
+        {
+            grid = new Grid<TilemapObject>(width, height, cellSize, originPosition,
+                (Grid<TilemapObject> g, int x, int y) => new TilemapObject(g,x,y));
+        }
+
+        public Grid<TilemapObject> GetGrid()
+        {
+            return this.grid;
+        }
+
+        public void SetTilemapSprite(Vector3 worldPosition, TilemapObject.TilemapSprite tilemapSprite)
+        {
+            TilemapObject tilemapObject= grid.GetGridObject(worldPosition);
+            if (tilemapObject != null)
+                tilemapObject.SetTilemapSprite(tilemapSprite);
+        }
+
+    }
+
+    public class TilemapObject
+    {
+        public enum TilemapSprite
+        {
+            None,
+            Ground
+        }
+
+        private Grid<TilemapObject> grid;
+        private int x, y;
+
+        private TilemapSprite tilemapSprite;
+
+        public TilemapObject(Grid<TilemapObject> grid, int x, int y)
+        {
+            this.grid = grid;
+            this.x = x;
+            this.y = y;
+        }
+
+        public void SetTilemapSprite (TilemapSprite tilemap)
+        {
+            this.tilemapSprite = tilemap;
+            grid.TriggerGridObjectChanged(x,y);
+        }
+
+        public TilemapSprite GetTilemapSprite()
+        {
+            return this.tilemapSprite;
+        }
+
+        public override string ToString()
+        {
+            return this.tilemapSprite.ToString();
+        }
+    }
+}
